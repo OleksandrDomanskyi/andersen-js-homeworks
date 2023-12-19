3abacae83341e038e206c230dedd722c208b697a
Oleksandr Domanskyi <oleksandrdomanskyi@gmail.com>
2023-12-19 22:18:36 +0200

    + first homework final solution

---
+++ 01_lesson_task/index.js
@@ -0,0 +1,47 @@
+// Global constants
+const INVALID_INPUT_MESSAGE = "Некорректный ввод!";
+const FIRST_MESSAGE = "Введите первое числовое значение:";
+const SECOND_MESSAGE = "Введите второе числовое значение:";
+
+// Function to prompt user for a numeric value
+const promptForNumber = (message) => {
+    const userInput = prompt(message);
+    const userNumber = +userInput;
+
+    return isNaN(userNumber) ? null : userNumber;
+};
+
+// Task 1
+const convertNumbers = () => {
+    const firstNumber = promptForNumber(FIRST_MESSAGE);
+    const secondNumber = promptForNumber(SECOND_MESSAGE);
+
+    if (firstNumber !== null && secondNumber !== null) {
+        console.log(firstNumber.toString(secondNumber));
+    } else {
+        console.log(INVALID_INPUT_MESSAGE);
+    }
+};
+
+convertNumbers();
+
+// Task 2
+const additionAndDivision = () => {
+    const firstNumber = promptForNumber(FIRST_MESSAGE);
+
+    if (firstNumber === null) {
+        console.log(INVALID_INPUT_MESSAGE);
+    } else {
+        const secondNumber = promptForNumber(SECOND_MESSAGE);
+
+        if (secondNumber === null) {
+            console.log(INVALID_INPUT_MESSAGE);
+        } else {
+            const addition = firstNumber + secondNumber;
+            const division = firstNumber / secondNumber;
+            console.log(`Ответ: ${addition}, ${division}.`);
+        }
+    } 
+}
+
+additionAndDivision();
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # andersen-js-homeworks
+
 Homework tasks &amp; my solutions for the Andersen's JS intensive course
+
+## To run the first app
+
+- `01_lesson_task` &mdash; go to the task folder.
+- `index.html` &mdash; drag the file onto a new browser tab.
