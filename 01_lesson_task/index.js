c3591699644b56e7b08c1e000a12efcde82af235
Oleksandr Domanskyi <oleksandrdomanskyi@gmail.com>
2023-12-23 21:09:19 +0200

    * some code changes for better experience

--- 01_lesson_task/index.js
+++ 01_lesson_task/index.js
@@ -2,13 +2,25 @@
 const INVALID_INPUT_MESSAGE = 'Некорректный ввод!';
 const FIRST_MESSAGE = 'Введите первое числовое значение:';
 const SECOND_MESSAGE = 'Введите второе числовое значение:';
+const MIN_RADIX_NUMBER = 2;
+const MAX_RADIX_NUMBER = 36;
 
 // Function to prompt user for a numeric value
 const promptForNumber = (message) => {
     const userInput = prompt(message);
+
+    if (userInput.trim() === "") {
+        return null;
+    }
+
     const userNumber = +userInput;
 
     return isNaN(userNumber) ? null : userNumber;
+};
+
+// Function to check code validity
+const isValidNumber = (number) => {
+    return number !== null && !isNaN(number);
 };
 
 // Task 1
@@ -16,13 +28,15 @@ const convertNumbers = () => {
     const firstNumber = promptForNumber(FIRST_MESSAGE);
     const secondNumber = promptForNumber(SECOND_MESSAGE);
 
-    if (firstNumber !== null && secondNumber !== null) {
-        console.log(firstNumber.toString(secondNumber));
-        return;
+    if (!isValidNumber(firstNumber) || !isValidNumber(secondNumber)) {
+        return console.log(INVALID_INPUT_MESSAGE);
     }
 
-    console.log(INVALID_INPUT_MESSAGE);
-    
+    if (secondNumber < MIN_RADIX_NUMBER || secondNumber > MAX_RADIX_NUMBER) {
+        return console.log(INVALID_INPUT_MESSAGE);
+    }
+
+    console.log(firstNumber.toString(secondNumber));
 };
 
 convertNumbers();
@@ -31,23 +45,20 @@ convertNumbers();
 const additionAndDivision = () => {
     const firstNumber = promptForNumber(FIRST_MESSAGE);
 
-    if (firstNumber === null) {
-        console.log(INVALID_INPUT_MESSAGE);
-        return;
+    if (!isValidNumber(firstNumber)) {
+        return console.log(INVALID_INPUT_MESSAGE);
     }
         
     const secondNumber = promptForNumber(SECOND_MESSAGE);
 
-    if (secondNumber === null) {
-        console.log(INVALID_INPUT_MESSAGE);
-        return;
+    if (!isValidNumber(secondNumber)) {
+        return console.log(INVALID_INPUT_MESSAGE);
     }
     
     const addition = firstNumber + secondNumber;
     const division = firstNumber / secondNumber;
+
     console.log(`Ответ: ${addition}, ${division}.`);
-
-
 }
 
 additionAndDivision();
